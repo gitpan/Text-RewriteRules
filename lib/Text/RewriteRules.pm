fece0e4c57a7233d26ee1672f1ff73a9f02cfeff
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2008-03-16 14:24:24 +0000

    Import of AMBS/Text-RewriteRules-0.12 from CPAN.
    gitpan-cpan-distribution: Text-RewriteRules
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         AMBS/Text/Text-RewriteRules-0.12.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

--- lib/Text/RewriteRules.pm
+++ lib/Text/RewriteRules.pm
@@ -12,11 +12,11 @@ Text::RewriteRules - A system to rewrite text using regexp-based rules
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 =head1 SYNOPSIS
 
